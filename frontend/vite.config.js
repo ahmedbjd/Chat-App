69188d948b8e31249eea47a879a871092f645fcb
ahmedbjd <ahmedbjd06@gmail.com>
2024-07-19 14:13:15 +0100

    update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     port: 3000,
     proxy: {
       "/api": {
-        target: 'https://chat-app-pk93.onrender.com/',
+        target: 'http://localhost:4000',
       }
     }
   }
