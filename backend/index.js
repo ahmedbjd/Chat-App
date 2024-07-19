b9fd49b2de49cbd500040c0358911e88ab5cbc1a
ahmedbjd <ahmedbjd06@gmail.com>
2024-07-19 11:55:29 +0100

    done

--- backend/index.js
+++ backend/index.js
@@ -2,14 +2,17 @@ import dotenv from "dotenv";
 import express from "express";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import path from "path";
 
 import authRoutes from "./routes/authRoutes.js"
 import ConnectToMongoDB from "./db/ConnectToMongoDB.js";
 import messageRoutes from "./routes/messageRoutes.js";
 import usersRoutes from "./routes/usersRoutes.js";
+import { app, server } from "./socket/socket.js";
 
-const app = express();
 const PORT = process.env.PORT || 4000;
+
+const __dirname = path.resolve();
 
 dotenv.config();
 
@@ -21,7 +24,13 @@ app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/users", usersRoutes);
 
-app.listen(PORT, () => {
+app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend/dist", "index.html"));
+})
+
+server.listen(PORT, () => {
     console.log(`Server runnung on port ${PORT}`)
     ConnectToMongoDB();
 });
