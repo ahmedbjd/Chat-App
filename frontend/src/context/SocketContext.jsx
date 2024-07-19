4cc9bd97343ab3da457e7fbc0bfec4ff594c46a4
ahmedbjd <ahmedbjd06@gmail.com>
2024-07-19 12:41:35 +0100

    update

--- frontend/src/context/SocketContext.jsx
+++ frontend/src/context/SocketContext.jsx
@@ -14,7 +14,7 @@ export const SocketContextProvider = ({ children }) => {
     const {authUser} = useAuth();
     useEffect(() => {
         if (authUser){
-            const socket = io("http://localhost:4000", {
+            const socket = io("https://chat-app-pk93.onrender.com", {
                 query: {
                     userId: authUser._id,
                 }
